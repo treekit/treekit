a132183b5f452a584db9b6e72cbc78d6acda1f8f
Sandro Santilli <strk@keybit.net>
2015-01-31 21:13:13 +0100

    Switch to TreeKIT-hosted tiles
    Closes #49

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,6 +1,7 @@
 (function(NS) {
   'use strict';
 
+  var access_token = 'pk.eyJ1IjoidHJlZWtpdCIsImEiOiJEVkk3Ym1ZIn0.QsmZ0FVaZAw9yfOBBg3qFg';
   NS.Config = {
     map: {
       center: [40.8343, -73.9279],
@@ -10,14 +11,14 @@
     },
     layers: [
       {
-        url: 'http://{s}.tiles.mapbox.com/v3/openplans.map-dmar86ym/{z}/{x}/{y}.png',
+        url: 'http://{s}.tiles.mapbox.com/v4/treekit.l36o6l2e/{z}/{x}/{y}.png?access_token='+access_token,
         attribution: '<a href="http://mapbox.com/about/maps" target="_blank">Terms &amp; Feedback</a>',
         maxZoom: 19
       }
     ],
     previewLayers: [
       {
-        url: 'http://{s}.tiles.mapbox.com/v3/openplans.gcp3ekpl/{z}/{x}/{y}.png',
+        url: 'http://{s}.tiles.mapbox.com/v4/treekit.l370il9f/{z}/{x}/{y}.png?access_token='+access_token,
         attribution: '<a href="http://mapbox.com/about/maps" target="_blank">Terms &amp; Feedback</a>',
         maxZoom: 19
       }
